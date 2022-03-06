1266247991cb33c59b51d94fd22c8346f0cb335e
Andrew Cauchi <drewcauchi@gmail.com>
2022-03-06 15:19:51 +0100

    Complete the last minor changes

--- js/main.js
+++ js/main.js
@@ -25,3 +25,37 @@ $('.single-work-link').on('mousemove', function(e) {
     $('.desktop-mouse-hover', this).css('top', `${clientY - top}px`)
     $('.desktop-mouse-hover', this).css('left', `${clientX - left}px`)
 })
+
+$(window).resize(function(){
+    location.reload();
+});
+
+if($(window).width() < 1060) {
+    $(window).scroll(function() {
+        var halfWindowHeight = $(window).height() / 2
+        var scrollDistance = $(window).scrollTop()
+        var workDistance = $('.works-sect').offset().top - halfWindowHeight
+        var aboutDistance = $('.about-sect').offset().top - halfWindowHeight
+        var contactDistance = $('.contact-sect').offset().top - halfWindowHeight
+
+        if(scrollDistance >= workDistance && scrollDistance < aboutDistance) {
+            $('.main-title').text('works')
+        } else if(scrollDistance >= aboutDistance && scrollDistance < contactDistance) {
+            $('.main-title').text('about')
+        } else if(scrollDistance >= contactDistance) {
+            $('.main-title').text('reach')
+        }
+    })
+} else {
+    $('.horizontal-scroller').scroll(function() {
+        var scrollDistance = $('.horizontal-scroller').scrollTop()
+
+        if(scrollDistance >= 0 && scrollDistance < 4400) {
+            $('.main-title').text('works')
+        } else if(scrollDistance >= 4400 && scrollDistance < 6100) {
+            $('.main-title').text('about')
+        } else if(scrollDistance >= 6100) {
+            $('.main-title').text('reach')
+        }
+    })
+}
